d9681f3431a65aeafd2431af8958c9ea9d7cc4a1
iremmmat <iremmmat@gmail.com>
2026-04-24 22:00:40 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/oohshelf/', // OohShelf
+  base: '/', 
   plugins: [react()],
   test: {
     environment: 'jsdom',
